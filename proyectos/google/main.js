16f3f58fe4fb62eabce38a5210678bc14b715ce3
Roman <romanvalles@gmail.com>
2023-02-15 15:08:29 -0300

    Update

--- proyectos/google/main.js
+++ proyectos/google/main.js
@@ -11,3 +11,62 @@ let search = document.getElementById("search");
 search.addEventListener("click", () => {
   historial.classList.toggle("action2");
 });
+
+let btnsearchHi = document.getElementById("button-search21");
+let entrada = document.getElementById("input");
+let ul = document.getElementById("historialul");
+let btnsearch = document.getElementById("button-search");
+
+btnsearchHi.addEventListener("click", () => {
+  let busqueda = entrada.value;
+
+  let sal = document.createElement("p");
+
+  let txt = document.createTextNode(busqueda);
+
+  sal.appendChild(txt);
+
+  ul.appendChild(sal);
+
+  entrada.value = "";
+});
+
+btnsearch.addEventListener("click", () => {
+  let busqueda = entrada.value;
+
+  let sal = document.createElement("p");
+
+  let txt = document.createTextNode(busqueda);
+
+  sal.appendChild(txt);
+
+  ul.appendChild(sal);
+
+  entrada.value = "";
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Enter") {
+    let busqueda = entrada.value;
+
+    let sal = document.createElement("p");
+
+    let txt = document.createTextNode(busqueda);
+
+    sal.appendChild(txt);
+
+    ul.appendChild(sal);
+
+    entrada.value = "";
+  }
+});
+
+let borrar = document.getElementById("borrar");
+
+let largo = entrada.value;
+
+console.log(largo);
+
+if (largo.length >= 1) {
+  borrar.style.display = "block";
+}
